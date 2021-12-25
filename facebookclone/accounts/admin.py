f4cac429c794204a3362edd5614161c93ba7a37b
minthing <minjeongandk@gmail.com>
2021-12-25 20:38:42 +0900

    accounts-admin
    - how to make superuser
    - how to handle database

--- facebookclone/accounts/admin.py
+++ facebookclone/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+# 데이터 베이스의 내용을 다룰 수 있는 툴
+
+#데코레이터
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+  list_display = ['id', 'nickname', 'user']
+  list_display_links = ['nickname', 'user']
+  search_fields = ['nickname']
